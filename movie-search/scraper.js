b03bb2837225a7d04094fc7361f5c85c7a876f96
sweezy793 <sarthaks793@gmail.com>
2018-08-15 00:00:26 +0530

    Companies

--- movie-search/scraper.js
+++ movie-search/scraper.js
@@ -42,14 +42,14 @@ function getMovie(imdbID){  //get the movie details of the particular movie's im
         const runtime=$('time[itemProp="duration"]').first().contents().filter(function(){
             return this.type==='text'; 
         }).text().trim();
-        const genres=[];
-        $('span[itemProp="genre"]').each(function(i,element){
-            const genre=$(element).text();
-            genres.push(genre);
-        });
+        
+        
         const releaseDate=$('meta[itemProp="datePublished"]').attr('content');
+        
         const imdbRating=$('span[itemProp="ratingValue"]').text();
+        
         const poster=$('div.poster a img').attr('src');
+        
         const plot=$('div.summary_text').text().trim();
         
 
@@ -60,15 +60,23 @@ function getMovie(imdbID){  //get the movie details of the particular movie's im
             }
         }
 
+        const genres=[];
+        $('span[itemProp="genre"]').each(getItems(genres));
 
         const directors=[];
         $('span[itemProp="director"]').each(getItems(directors));
+        
         const writers=[];
         $('.credit_summary_item span[itemProp="creator"]').each(getItems(writers));
+        
         const stars=[];
         $('.credit_summary_item span[itemProp="actors"]').each(getItems(stars));
         
         const storyline=$('#titleStoryLine span[itemProp="description"]').text().trim();
+
+        const companies = [];
+        $('span[itemType="http://schema.org/Organization"]').each(getItems(companies));
+        
         return{
             imdbID,
             title,
@@ -82,7 +90,8 @@ function getMovie(imdbID){  //get the movie details of the particular movie's im
             directors,
             writers,
             stars,
-            storyline
+            storyline,
+            companies
         };
     });
 }
