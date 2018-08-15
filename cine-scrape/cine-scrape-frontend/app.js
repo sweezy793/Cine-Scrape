d371734834f21ea7214c6183bfac8f21e339b84a
sweezy793 <sarthaks793@gmail.com>
2018-08-15 22:13:07 +0530

    Shows the list in frontend

--- cine-scrape/cine-scrape-frontend/app.js
+++ cine-scrape/cine-scrape-frontend/app.js
@@ -1,5 +1,6 @@
 const form=document.querySelector('form');
 const searchInput=document.querySelector('input');  
+const resultList=document.querySelector('#results');
 
 const BASE_URL='https://cine-scrapper.now.sh/';
 
@@ -9,13 +10,30 @@ function formSubmitted(event){
     event.preventDefault();//prevents a default function from happening i.e. submit button from submitting
 
     const searchTerm=searchInput.value;
-    getSearchResults(searchTerm);
+    getSearchResults(searchTerm)
+    .then(showResults);
 }
 
 function getSearchResults(searchTerm){
     return fetch(`${BASE_URL}search/${searchTerm}`)
-    .then(res=>res.json())
-    .then(results=>{
-        console.log(results);
-    });
+    .then(res=>res.json());
+}
+
+function showResults(results){
+    results.forEach(movie=>{
+        const li=document.createElement('li');
+        const br=document.createElement('br');
+        
+        const img=document.createElement('img');
+        li.appendChild(img);
+        
+        img.src=movie.image;
+        const span=document.createElement('span');
+        span.textContent=" "+movie.title;
+        
+        li.appendChild(span);
+        
+        resultList.appendChild(li);
+        resultList.appendChild(br);
+    })
 }
