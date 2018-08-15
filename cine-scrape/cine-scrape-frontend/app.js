f43901a873022c7630dac010cb478b6917c16768
sweezy793 <sarthaks793@gmail.com>
2018-08-15 21:18:45 +0530

    form fetches data from constructed API

--- cine-scrape/cine-scrape-frontend/app.js
+++ cine-scrape/cine-scrape-frontend/app.js
@@ -1,11 +1,21 @@
 const form=document.querySelector('form');
 const searchInput=document.querySelector('input');  
 
+const BASE_URL='https://cine-scrapper.now.sh/';
+
 form.addEventListener('submit',formSubmitted);
 
 function formSubmitted(event){
     event.preventDefault();//prevents a default function from happening i.e. submit button from submitting
 
     const searchTerm=searchInput.value;
-    console.log(searchTerm);
+    getSearchResults(searchTerm);
+}
+
+function getSearchResults(searchTerm){
+    return fetch(`${BASE_URL}search/${searchTerm}`)
+    .then(res=>res.json())
+    .then(results=>{
+        console.log(results);
+    });
 }
