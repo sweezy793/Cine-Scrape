9a90d303471ed998f22b3ac189c766023bd4a3e3
sweezy793 <sarthaks793@gmail.com>
2018-08-16 00:45:11 +0530

    Almost done

--- cine-scrape/cine-scrape-frontend/movie.js
+++ cine-scrape/cine-scrape-frontend/movie.js
@@ -7,36 +7,52 @@ function getMovie(imdbID){
     .then(res=>res.json());
 }
 
-const properties = [{
-    title: 'Rating',
-    property: 'rating'
-  }, {
-    title: 'Run Time',
-    property: 'runTime'
-  }, {
-    title: 'Released',
-    property: 'datePublished'
-  }, {
-    title: 'Summary',
-    property: 'summary'
-  }, {
-    title: 'Story Line',
-    property: 'storyLine'
-  }];
+
 
 function showMovie(movie){
     console.log(movie);
     const section=document.createElement('section');
     main.appendChild(section);
 
-    section.outerHTML=`
-    <section class="row">
-    <h1>${movie.title}</h1>
-        <div class="col-sm-12">
-            <img src="${movie.poster}" class="img-fluid"/>
+    const properties = [{
+        title: 'Rating',
+        property: 'rating'
+      }, {
+        title: 'Run Time',
+        property: 'runtime'
+      }, {
+        title: 'Released',
+        property: 'releaseDate'
+      }, {
+        title: 'Summary',
+        property: 'plot'
+      }, {
+        title: 'Story Line',
+        property: 'storyline'
+      }];
+    
+      const descriptionHTML = properties.reduce((html, property) => {
+        html += `
+          <dt class="col-sm-3">${property.title}</dt>
+          <dd class="col-sm-9">${movie[property.property]}</dd>`;
+        return html;
+      }, '');
+
+      section.outerHTML = `
+      <section class="row">
+        
+        <div class="col-sm-5">
+        <h3 class="text-center">${movie.title}</h3>
+          <img src="${movie.poster}" class="img-fluid" />
         </div>
-    </section>`
-}
+        <div class="col-sm-7">
+          <dl class="row" style="margin-top:12em">
+            ${descriptionHTML}
+          </dl>
+        </div>
+      </section>
+    `;
+  }
 
 getMovie(imdbID)
 .then(showMovie)
